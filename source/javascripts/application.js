0cf3a341e0795c8d6b912f80b9ba837540bc2b53
stan wiart <stanislas.wiart@gmail.com>
2017-10-31 20:06:01 +0100

    add avara

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -55,6 +55,6 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid rgba(0,0,0,1); padding-right: 5vh; }";
   document.body.appendChild(css);
 };
